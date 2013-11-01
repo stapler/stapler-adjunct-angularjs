53461200f6c0c71fcb8f80d92d618ae4f3cdd72c
rsandell <sandell.robert@gmail.com>
2013-11-01 22:58:38 +0100

    Latest RC

--- import.sh
+++ import.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-tag=1.0.8
+tag=1.2.0-rc.3
 dst=src/main/resources/org/kohsuke/stapler/angularjs
 wget -O $dst/angular.js     https://ajax.googleapis.com/ajax/libs/angularjs/$tag/angular.js
 wget -O $dst/angular.min.js https://ajax.googleapis.com/ajax/libs/angularjs/$tag/angular.min.js
